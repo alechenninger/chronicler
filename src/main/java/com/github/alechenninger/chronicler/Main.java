38bee7c80669eb0ee759f1ddfa0f7718e7c48744
Alec Henninger <ahenning@redhat.com>
2015-07-18 15:09:07 -0400

    Correct version

--- src/main/java/com/github/alechenninger/chronicler/Main.java
+++ src/main/java/com/github/alechenninger/chronicler/Main.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 
 public abstract class Main {
   private static final ConfigFactory configFactory = new ConfigFactory();
-  private static final String VERSION = "3.0.0";
+  private static final String VERSION = "3.1.1";
 
   public static void main(String[] args) throws Exception {
     ChroniclerConfig config = configFactory.fromCommandLine(args);
